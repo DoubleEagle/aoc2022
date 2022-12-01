ff01544b0461426bd9db6786167f5a88bcf8bfa2
Joanne Spijker <spijkerjj@gmail.com>
2022-12-01 22:09:34 +0100

    Added startup script, it's not pretty but it works

---
+++ start.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+print("Enter day number:")
+
+day_num = input()
+
+print("Creating files for day ", day_num, "...")
+
+os.mkdir("day" + day_num)
+
+f = open("day" + day_num + "/day" + day_num + ".py", "a")
+f.write("input_file = open('day" + day_num + "_input.txt')\n"
+        "\n"
+        "input = input_file.read().split('\\n')")
+f.close()
+
+f_dummy = open("day" + day_num + "/day" + day_num + "_dummyinput.py", "a")
+f_dummy.close()
+
+f_input = open("day" + day_num + "/day" + day_num + "_input.py", "a")
+f_input.close()

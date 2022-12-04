a1852110b42ad8bd64157ea4bba8893272a42d96
Joanne Spijker <spijkerjj@gmail.com>
2022-12-04 12:28:39 +0100

    Fixed start script

--- start.py
+++ start.py
@@ -3,20 +3,21 @@ import shutil
 
 print("Enter day number:")
 
-day_num = input()
+day_num = str(input())
 
 print("Creating files for day ", day_num, "...")
 
 os.mkdir("day" + day_num)
 
 f = open("day" + day_num + "/day" + day_num + ".py", "a")
-f.write("input_file = open('day" + day_num + "_input.txt')\n"
+f.write("input_file = open('day" + day_num + "_dummyinput.txt')\n"
+        "# input_file = open('day" + day_num + "_input.txt')\n"
         "\n"
         "input = input_file.read().split('\\n')")
 f.close()
 
-f_dummy = open("day" + day_num + "/day" + day_num + "_dummyinput.py", "a")
+f_dummy = open("day" + day_num + "/day" + day_num + "_dummyinput.txt", "a")
 f_dummy.close()
 
-f_input = open("day" + day_num + "/day" + day_num + "_input.py", "a")
+f_input = open("day" + day_num + "/day" + day_num + "_input.txt", "a")
 f_input.close()

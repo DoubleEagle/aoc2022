5d8b921e66f25c257479528ad711b0b091014d5d
Joanne Spijker <spijkerjj@gmail.com>
2022-12-07 17:58:49 +0100

    Day 7, at long last

--- day7/day7.py
+++ day7/day7.py
@@ -1,14 +1,15 @@
-input_file = open('day7_dummyinput.txt')
-# input_file = open('day7_input.txt')
+# input_file = open('day7_dummyinput.txt')
+input_file = open('day7_input.txt')
 
 input = input_file.read().split('\n')
 
 filesystem_tree = {'/': {}}
 current_location = ['/']
+all_sizes = []
+sizes_under_100000 = []
 
 
-def pretty_print_tree(tree, prefix="\t"):
-    # print("=== pretty print", tree, prefix)
+def pretty_print_tree(tree, prefix=""):
     for key, value in tree.items():
         if type(value) is dict:
             print(prefix + "- " + key)
@@ -17,55 +18,86 @@ def pretty_print_tree(tree, prefix="\t"):
             print(prefix + "- " + str(key) + " (size " + str(value) + ")")
 
 
-def find_current_directory_contents(location, tree):
-    # print("=== finding directory...", location, tree)
-    if len(location) > 1:
-        return find_current_directory_contents(location[1:], tree[location[0]])
-    else:
-        # print("returning tree... ", tree)
-        return tree[location[0]]
+def insert_directory(path, tree, dir_name):
+    if len(path) <= 1:
+        tree[path[0]][dir_name] = {}
+        return tree
+    for directory, contents in tree.items():
+        if directory == path[0]:
+            tree[directory] = insert_directory(path[1:], tree[directory], dir_name)
+    return tree
 
 
-def add_to_tree(location, tree):
-    if len(location) > 1:
-        return add_to_tree(location[1:], tree[location[0]])
-    else:
-        return
+def insert_file(path, tree, file_name, size):
+    if len(path) <= 1:
+        tree[path[0]][file_name] = int(size)
+        return tree
+    for directory, contents in tree.items():
+        if directory == path[0]:
+            tree[directory] = insert_file(path[1:], tree[directory], file_name, size)
+    return tree
 
 
 def parse_command(command, state, location):
     if command[:2] != "$ ":
         print("This is not a command!")
         exit()
-    if command == "$ ls":
-        print("> reading directory \n")
+    # if command == "$ ls":
+    #     print("> reading directory \n")
     elif "$ cd " in command:
-        print("> change directory \n")
+        # print("> change directory \n")
         new_location = command.split(" ")[2]
         if new_location == "..":
             location = location[:-1]
         else:
+            # check if already in tree and if not add it
+            state = insert_directory(location, state, new_location)
             # change location
             location.append(new_location)
-            # check if already in tree and if not add it
-            state
+    return state, location
 
 
-for line in input:
-    # print(line)
+def globally_calculate_directory_sizes(tree):
+    global all_sizes
+    size = 0
+    for directory, contents in tree.items():
+        if type(contents) is int:
+            size += contents
+        else:
+            d_s = globally_calculate_directory_sizes(contents)
+            size += d_s
+            all_sizes.append(d_s)
+            if d_s <= 100000:
+                sizes_under_100000.append(d_s)
+    return size
+
+
+for line in input[1:]:
     if line.startswith("$ "):
-        print("Command found:", line)
         filesystem_tree, current_location = parse_command(line, filesystem_tree, current_location)
     elif line.startswith("dir "):
         # add directory under current directory
-        print("Directory found:", line)
+        insert_directory(current_location, filesystem_tree, line.split(' ')[1])
     elif line.split(" ")[0].isnumeric():
-        print("File found:", line)
+        size = line.split(" ")[0]
+        file_name = line.split(" ")[1]
+        filesystem_tree = insert_file(current_location, filesystem_tree, file_name, size)
     else:
         print("Something unparsable found:", line)
         exit()
 
+pretty_print_tree(filesystem_tree)
 
-test_tree = {'/': {'a': {'e': {'i': 120}}, 'b': 14848514, 'c': 14848514, 'd': {'j': 4060174}}}
-# pretty_print_tree(test_tree)
-print(find_current_directory_contents(['/', 'a', 'e'], test_tree))
+total_size = globally_calculate_directory_sizes(filesystem_tree)
+
+print("Part 1:", sum(sizes_under_100000))
+
+# This is total beun, but at this point I was done with it
+total_disk_space = 70000000
+at_least_free = 30000000
+max_size = total_disk_space - at_least_free
+
+space_to_free = total_size - max_size
+
+candidates = filter(lambda x: (x >= space_to_free), all_sizes)
+print("Part 2:", min(list(candidates)))
